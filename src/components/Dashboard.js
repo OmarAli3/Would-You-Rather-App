8678292a5c1947d539137e719e9414a37b0f6978
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2021-04-07 14:57:15 +0200

    Adding dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,59 @@
 import { React, Component } from "react";
+import { connect } from "react-redux";
+
 class Dashboard extends Component {
+    state = {
+        answeredList: false,
+    };
+    handleDisplayedList = (answeredList) => this.setState({ answeredList });
+
     render() {
         return (
             <div className="dashboard">
                 <nav className="question-nav">
                     <ul>
-                        <li>Unanswered Questions</li>
-                        <li>Answered Questions</li>
+                        <li
+                            onClick={() => this.handleDisplayedList(false)}
+                            className={
+                                this.state.answeredList ? "" : "active-li"
+                            }
+                        >
+                            Unanswered Questions
+                        </li>
+                        <li
+                            onClick={() => this.handleDisplayedList(true)}
+                            className={
+                                this.state.answeredList ? "active-li" : ""
+                            }
+                        >
+                            Answered Questions
+                        </li>
                     </ul>
                 </nav>
+                <ul className="dashboard-list">
+                    {this.state.answeredList
+                        ? this.props.answeredQuetionIds.map((id) => (
+                              <li key={id}>{id}</li>
+                          ))
+                        : this.props.unansweredQuetionIds.map((id) => (
+                              <li key={id}>{id}</li>
+                          ))}
+                </ul>
             </div>
         );
     }
 }
 
-export default Dashboard;
+const mapStatetToProps = ({ questions, authedUser, users }) => {
+    const answers = authedUser ? Object.keys(users[authedUser].answers) : [];
+    return {
+        answeredQuetionIds: answers.sort(
+            (a, b) => questions[a].timestamp - questions[b].timestamp
+        ),
+        unansweredQuetionIds: Object.keys(questions)
+            .filter((id) => !answers.includes(id))
+            .sort((a, b) => questions[a].timestamp - questions[b].timestamp),
+    };
+};
+
+export default connect(mapStatetToProps)(Dashboard);
