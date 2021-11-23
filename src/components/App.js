05fd6bb8d3bcb08d473daa48dfe50f4440726834
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2021-11-23 22:17:07 +0200

    Adding github pages

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     }
     render() {
         return (
-            <BrowserRouter>
+            <BrowserRouter basename="/Would-You-Rather-App/">
                 <div className="App">
                     <LoadingBar />
                     <Navbar />
